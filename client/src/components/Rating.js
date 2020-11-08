4a92b224e725791c8fc7b7a0c1748e75f6cc21f2
Lazar <layarkostic94@gmail.com>
2020-11-08 15:30:13 +0100

    Added and Implemented react-router-dom

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -67,6 +68,12 @@ const Rating = ({ value, text, color }) => {
 
 Rating.defaultProps = {
     color: '#f8e825'
+}
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string
 }
 
 export default Rating;
