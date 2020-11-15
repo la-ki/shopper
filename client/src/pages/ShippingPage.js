dd809ab272cef489f0e9692f2390f105942666da
Lazar <layarkostic94@gmail.com>
2020-11-15 16:42:49 +0100

    Added Payment Page and Save Payment Method

--- client/src/pages/ShippingPage.js
+++ client/src/pages/ShippingPage.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
-import {saveShippingAddress} from '../actions/cartActions';
+import { saveShippingAddress } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingPage = ({ history }) => {
   const cart = useSelector((state) => state.cart);
-  const {shippingAddress} = cart;
+  const { shippingAddress } = cart;
 
   const [address, setAddress] = useState(shippingAddress.address);
   const [city, setCity] = useState(shippingAddress.city);
@@ -17,12 +18,13 @@ const ShippingPage = ({ history }) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(saveShippingAddress({address, city, postalCode, country}));
-    history.push('/payment');
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    history.push("/payment");
   };
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
@@ -69,7 +71,7 @@ const ShippingPage = ({ history }) => {
           ></Form.Control>
         </Form.Group>
 
-        <Button type="submit" variant="primary" className="btn-block">
+        <Button type="submit" className="btn-block" variant="primary">
           Continue
         </Button>
       </Form>
