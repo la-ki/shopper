6dae483df41e45707a01a8d80380e6dcf6c38222
Lazar <donickoce@gmail.com>
2020-12-01 20:04:32 +0100

    Added Top Rated Product to Carousel

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -135,6 +135,15 @@ const createProductReview = asyncHandler(async (req, res) => {
     res.status(404)
     throw new Error('Product not found')
   }
+});
+
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({rating: -1}).limit(3);
+
+  res.json(products);
 })
 
-export { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview };
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview, getTopProducts };
